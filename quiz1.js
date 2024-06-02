1b57066d07025f2ccf57d4d7537b6515f566f6c6
Mohit Bora <boramohit3@gmail.com>
2024-06-02 18:58:14 +0530

    Objects

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+
+let obj = {
+
+    "Name":"Mohit Bora",
+    "Uid":"21BCS5364",
+    "Stream":"B.E-CSE"
+
+}
+
+obj.Number=7417494748
+console.log(obj)
+
+console.log(typeof obj.Name,typeof obj.Number);
